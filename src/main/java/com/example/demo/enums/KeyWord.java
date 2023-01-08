83374fbd7ff4c204f4ac828371726f6af9202873
dilsoncampelo10 <dilsono316@gmail.com>
2023-01-07 22:43:35 -0300

    criando model project e enum de palavras chaves

---
+++ src/main/java/com/example/demo/enums/KeyWord.java
@@ -0,0 +1,18 @@
+package com.example.demo.enums;
+
+public enum KeyWord {
+	
+	TECNOLOGIA("Informática"),
+	ROBOTICA("Robótica"),
+	FISICA("Física"),
+	PROGRAMACAO("Programação"),
+	QUIMICA("Química"),
+	BIOLOGIA("Biologia");
+	
+	private String keyWord;
+	
+	private KeyWord(String keyWord) {
+		this.keyWord = keyWord;
+	}
+
+}
---
+++ src/main/java/com/example/demo/models/Project.java
@@ -0,0 +1,58 @@
+package com.example.demo.models;
+
+import com.example.demo.enums.KeyWord;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Project {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	private String title;
+	
+	private String abstractProject;
+	
+	private KeyWord keyWord;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAbstractProject() {
+		return abstractProject;
+	}
+
+	public void setAbstractProject(String abstractProject) {
+		this.abstractProject = abstractProject;
+	}
+
+	public KeyWord getKeyWord() {
+		return keyWord;
+	}
+
+	public void setKeyWord(KeyWord keyWord) {
+		this.keyWord = keyWord;
+	}
+	
+	
+	
+
+}
