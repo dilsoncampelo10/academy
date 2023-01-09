bec7ca59257da62e0be779f1589df889c81f069a
dilsoncampelo10 <dilsono316@gmail.com>
2023-01-09 14:47:02 -0300

    Adicionando funcionalidade de editar

--- src/main/java/com/example/demo/controllers/ProjectController.java
+++ src/main/java/com/example/demo/controllers/ProjectController.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,8 +26,35 @@ public class ProjectController {
 	@PostMapping("/project/create")
 	public ModelAndView insert(Project project) {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("redirect:/");
+		mv.setViewName("redirect:show");
 		projectRepository.save(project);
+		return mv;
+	}
+	
+	@GetMapping("/project/show")
+	public ModelAndView show() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("project/project_show");
+		mv.addObject("projects", projectRepository.findAll());
+		return mv;
+	}
+	
+	@GetMapping("/project/edit/{id}")
+	public ModelAndView edit(@PathVariable("id") Integer id) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("project/project_edit");
+		Project project = projectRepository.getReferenceById(id);
+		mv.addObject("project",project);
+		
+		return mv;
+		
+	}
+	
+	@PostMapping("/project/edit")
+	public ModelAndView update(Project project) {
+		ModelAndView mv = new ModelAndView();
+		projectRepository.save(project);
+		mv.setViewName("redirect:show");
 		return mv;
 	}
 }
